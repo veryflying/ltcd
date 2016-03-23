445d4c5c7b5cc7ebee6b092b860653b9d380d258
veryflying <veryflying@gmail.com>
2016-03-23 22:45:23 +0800

    [Time Limit Exceeded]committed by LeetCode Extension

---
+++ 70-Climbing-Stairs/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        t = [None] * (n+1)
+        def s(i):
+            if i >= n-1:
+                t[i] = n - i
+                return n - i
+            t[i] = s(i+1) + s(i+2) +1
+            return t[i]
+        return s(0)
