60bc8112447ac6222a11671fe0524238d53f787e
veryflying <veryflying@gmail.com>
2016-03-23 22:51:24 +0800

    [Accepted]fib

--- 70-Climbing-Stairs/70.py
+++ 70-Climbing-Stairs/70.py
@@ -8,9 +8,9 @@ class Solution(object):
         def s(i):
             if t[i] is not None:
                 return t[i]
-            if i >= n-1:
+            if i >= n-2:
                 t[i] = n - i
                 return n - i
-            t[i] = s(i+1) + s(i+2) +1
+            t[i] = s(i+1) + s(i+2)
             return t[i]
         return s(0)
