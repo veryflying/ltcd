2ce7348fb25d5f11b539b6ad218677a905e71161
veryflying <veryflying@gmail.com>
2016-03-24 14:35:36 +0800

    [Runtime Error]committed by LeetCode Extension

---
+++ 73-Set-Matrix-Zeroes/73.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        tmp = []
+        for i in xrange(len(matrix)):
+            for j in xrange(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    tmp.append((i, j))
+        for i in tmp:
+            for j in xrange(len(matrix[0])):
+                matrix[i][j] = 0
+            for j in xrange(len(matrix)):
+                matrix[j][i] = 0
