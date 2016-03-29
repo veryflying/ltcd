03688155e610ed23d0ff77643bc277d77c06dac6
veryflying <veryflying@gmail.com>
2016-03-29 15:25:30 +0800

    [82. Remove Duplicates from Sorted List II][Runtime Error]committed by LeetCode Extension

---
+++ 82-Remove-Duplicates-from-Sorted-List-II/solution.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        def delete_next(node_i):
+            t = node_i.next
+            node_i.next = t.next
+        p = head
+        while p.next is not None:
+            if p.val == p.next.val:
+                delete_next(p)
+        return head
